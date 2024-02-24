81494e50abe2d5fec698139107800d19ed04b817
Seedling <david.r.graham32@gmail.com>
2024-02-24 12:36:05 -0500

    Display salamander name and id in foreach table. Use url_for and h() with show.php

--- phase01/public/salamanders/index.php
+++ phase01/public/salamanders/index.php
@@ -35,16 +35,16 @@
 
       <?php foreach($salamanders as $salamander) { ?>
         <tr>
-          <!-- <td>Display the salamander id</td> -->
-    	    <!-- <td>Display the salamander name</td> -->
-          <!-- Use url_for with show.php AND h(u) with the salamander['id'] -->
+          <td><?php echo $salamander['id']; ?></td>
+    	    <td><?php echo $salamander['salamanderName']; ?></td>
+          <td><a href="<?= url_For('/salamanders/show.php?id=' . $salamander['id']); ?>">View</a></td>
           <td><a href="#">Edit</a></td>
           <td><a href="#">Delete</a></td>
     	  </tr>
       <?php } ?>
   	</table>
 
-<?
+<?php
   // php add the shared path to the salamander footer 
   include(SHARED_PATH . '/salamander-footer.php');
 
